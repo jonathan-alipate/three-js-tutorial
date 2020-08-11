bcacc611297873a83279c6856ed5693d73d9de40
Jonathan Alipate <j.alipate@gmail.com>
2020-08-11 23:38:25 +1200

    updating var to let

--- three.js
+++ three.js
@@ -1,18 +1,18 @@
 let scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-var renderer = new THREE.WebGLRenderer();
+let renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry();
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-var cube = new THREE.Mesh(geometry, material);
+let geometry = new THREE.BoxGeometry();
+let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+let cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 camera.position.z = 5;
 
-var animate = function () {
+let animate = function () {
     requestAnimationFrame(animate);
 
     cube.rotation.x += 0.01;
